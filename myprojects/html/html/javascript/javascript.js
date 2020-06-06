848e604ed0d7fdf5bee737e8fbbf76507238cb0e
kachaj96 <drozdzynskiadrian@gmail.com>
2020-06-06 14:36:30 +0200

    Added JavaScript page to test JS

---
+++ myprojects/html/html/javascript/javascript.js
@@ -0,0 +1,12 @@
+// funkcja odpowiadająca za znikanie obrazu w index.html
+function imageGone() {
+    document.getElementById("imageGoneID").innerHTML = "Image is gone!";
+}
+
+//  Writing into an HTML element, using innerHTML.
+//  Writing into the HTML output using document.write().
+//  Writing into an alert box, using window.alert().
+//  Writing into the browser console, using console.log().
+
+//  To access an HTML element, JavaScript can use the document.getElementById(id) method.
+//  The id attribute defines the HTML element. The innerHTML property defines the HTML content.
