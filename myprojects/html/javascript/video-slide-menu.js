b155797687d9d1d0cc4b0f31f385088383e29a2b
kachaj96 <drozdzynskiadrian@gmail.com>
2020-06-07 13:28:49 +0200

    Added left-slide menu with Video after clickon button

---
+++ myprojects/html/javascript/video-slide-menu.js
@@ -0,0 +1,9 @@
+/* otwiera wysuwane menu jak ktoś kliknie w element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+  
+/* zamyka jak ktoś kliknie na X */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
