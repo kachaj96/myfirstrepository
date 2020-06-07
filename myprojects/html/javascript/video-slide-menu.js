afe62bfdf3d87f6903d081d867838be90b0171b7
kachaj96 <drozdzynskiadrian@gmail.com>
2020-06-07 15:44:56 +0200

    Contact page in progress

--- myprojects/html/javascript/video-slide-menu.js
+++ myprojects/html/javascript/video-slide-menu.js
@@ -1,9 +1,9 @@
 /* otwiera wysuwane menu jak ktoś kliknie w element */
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+    document.getElementById("video-slide").style.width = "100%";
 }
   
 /* zamyka jak ktoś kliknie na X */
 function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+    document.getElementById("video-slide").style.width = "0%";
 }
