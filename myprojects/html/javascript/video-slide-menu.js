0672bb2d2b5b74858875d37bde4b0323c39848df
kachaj96 <drozdzynskiadrian@gmail.com>
2020-06-11 23:27:31 +0200

    2020

--- myprojects/html/javascript/video-slide-menu.js
+++
@@ -1,9 +0,0 @@
-/* otwiera wysuwane menu jak ktoś kliknie w element */
-function openNav() {
-    document.getElementById("video-slide").style.width = "100%";
-}
-  
-/* zamyka jak ktoś kliknie na X */
-function closeNav() {
-    document.getElementById("video-slide").style.width = "0%";
-}
